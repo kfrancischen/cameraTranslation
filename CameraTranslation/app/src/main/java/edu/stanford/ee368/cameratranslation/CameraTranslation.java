01bd5d1ea1091a9fd17abb02d295e89d10a21bab
francischen <kfrancischen@gmail.com>
2016-11-14 21:33:33 -0800

    add all

---
+++ CameraTranslation/app/src/main/java/edu/stanford/ee368/cameratranslation/CameraTranslation.java
@@ -0,0 +1,12 @@
+package edu.stanford.ee368.cameratranslation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraTranslation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
