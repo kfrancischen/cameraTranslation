63ff22bb7b69adc751b544a0b7dbbcfdc058425a
francischen <kfrancischen@gmail.com>
2016-11-15 23:29:15 -0800

    update code

--- CameraTranslation/app/src/main/java/edu/stanford/ee368/cameratranslation/CameraTranslation.java
+++ CameraTranslation/app/src/main/java/edu/stanford/ee368/cameratranslation/CameraTranslation.java
@@ -1,12 +1,33 @@
 package edu.stanford.ee368.cameratranslation;
 
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CameraTranslation extends AppCompatActivity {
+public class CameraTranslation extends AppCompatActivity implements CvCameraViewListener2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause(){
+
+    }
+
+    @Override
+    public void onResume(){
+
+    }
+
+    publid void onDestroy(){
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame){
+
     }
 }
